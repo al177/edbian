5f4e047fa91057ce3fde7355e96dad266215efa4
ajlitt <ajlitt@chairface>
2015-01-07 17:18:06 -0600

    start moving to dh_make / dpkg-buildpackage for everything

--- packager.sh
+++ packager.sh
@@ -21,13 +21,34 @@ function build_basic_pkg() {
 	fakeroot dpkg-deb --build ${PKG_NAME}
 }
 
+# build_src_pkg_start
 function build_src_pkg() {
 	echo "in build_src_pkg"
 	echo "pkgname = $PKG_NAME"
-	cp -a DEBIAN ${PKG_NAME}
-	sed -i -e "s/##VERSION##/${EDSRC_VER}/g" ${PKG_NAME}/DEBIAN/control
-	fakeroot dpkg-deb --build ${PKG_NAME}
+	pushd $PKG_NAME
+	dh_make -y --createorig -p ${PKG_NAME} -s
+	export QUILT_PATCHES=debian/patches
+	export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
+	mkdir ${PKG_NAME}/${QUILT_PATCHES}
+	if [ -e ../patches ]; then
+		for PATCH in `ls -1 ../patches`; do
+			quilt import ../patches/$PATCH
+		done
+		quilt import ${QUILT_PATCHES}
+	fi
+	if [ -e ../files ]; then
+		quilt new addfiles-${PKG_NAME}.patch
+		for FILE in `ls -1 ../files`; do
+			quilt add ../files/$FILE
+		done
+		quilt refresh
+		quilt header -e <<<"add files "
+	fi
+	quilt pop -a
+	dpkg-buildpackage -us -uc
+	popd
 }
+
 
 pushd $PKG_SKEL_DIR
 for CUR_PKG in *; do
